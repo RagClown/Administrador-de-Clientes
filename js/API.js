e0d5d49a7a1eafa844c91fe16b28aec3c1998abb
Gabriel Prato <pratogabriel@outlook.com>
2022-11-18 23:14:17 -0400

    First Commit

---
+++ js/API.js
@@ -0,0 +1,79 @@
+const url = 'http://localhost:3000/clientes';
+
+
+//Cuando se crea nuevo cliente
+export const nuevoCliente = async cliente =>{
+    
+    try {
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//Obtiene todos los clientes
+export const obtenerClientes = async () =>{
+
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Elimina cliente
+
+export const eliminarCliente = async (id)=>{
+
+    try {
+        await fetch(`${url}/${id}`,{
+            method: 'DELETE'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+//OBtiene un cliente por su ID
+
+export const obtenerCliente = async id =>{
+
+    try {
+        const resultado = await fetch(`${url}/${id}`)
+        const cliente = await resultado.json();
+        return cliente
+    } catch (error) {
+        console.log(error)
+    }
+} 
+
+//Actualiza un registro
+
+export const editarCliente = async cliente =>{
+    console.log(cliente)
+    try {
+        await fetch(`${url}/${cliente.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(cliente),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error)
+    }
+
+} 
